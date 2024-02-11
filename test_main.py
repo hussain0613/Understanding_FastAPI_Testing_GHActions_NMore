0de8097c39da4f065621c310089fc73bed7c3a32
Muhammad Hossain <hussain0613@gmail.com>
2024-02-11 20:49:21 +0600

    Updated `test_main.py` - overridden the `get_db_engine` dependency to use the `engine` created in the test file - updated the `clean_up` code to dispose the engine before trying to delete the db file.

--- test_main.py
+++ test_main.py
@@ -1,14 +1,18 @@
 from fastapi.testclient import TestClient
-from app import app_factory
-from schemas import ItemBase, ItemResponse
+from app import app_factory, get_db_engine
+from schemas import ItemResponse
 from init_db import Base
 from sqlalchemy import create_engine
 from settings import Settings
 
-settings: Settings = Settings("sqlite:///./test.db")
+sqlite_db_file = "test.db"
+settings: Settings = Settings(database_url=f"sqlite:///{sqlite_db_file}")
 engine = create_engine(settings.database_url)
 
 app = app_factory(settings)
+app.dependency_overrides[get_db_engine] = lambda: engine
+# if we use the factory function, then the `engine` object created by the `get_db_engine` function still be in the memory and will not be disposed of
+# and we will not be able to delete the database file
 
 client = TestClient(app)
 
@@ -18,6 +22,10 @@ items_to_create = [
     ItemResponse(name="Bar", description="Brawlers", price=200.0, id=2),
     ItemResponse(name="Baz", description="Blasters", price=300.0, id=3),
 ]
+
+def test_engine_settings():
+    assert settings.database_url == f"sqlite:///{sqlite_db_file}"
+    assert sqlite_db_file == engine.url.database
 
 
 def test_drop_db():
@@ -35,9 +43,6 @@ def test_create_item():
         "/items/",
         json=items_to_create[0].model_dump()
     )
-    print(response.json())
-    print(response)
-    print(response.status_code)
     assert response.status_code == 201
     assert response.json() == items_to_create[0].model_dump()
 
@@ -108,7 +113,26 @@ def test_read_items():
 
 def test_clean_up():
     Base.metadata.drop_all(engine)
+    engine.dispose() # this is important to dispose of the engine object, so that the database file can be deleted
+    
     import os
-    os.remove("./test.db")
-    print("Database dropped!")
+    os.remove(sqlite_db_file)
+    print("Cleaned up!")
+
+
+if __name__ == "__main__":
+    test_engine_settings()
+    test_drop_db()
+    test_init_db()
+    test_create_item()
+    test_create_item_2()
+    test_create_item_3()
+    test_create_duplicate_item()
+    test_read_item()
+    test_read_item_2()
+    test_read_item_3()
+    test_read_item_not_found()
+    test_read_items()
+    test_clean_up()
+    print("All tests passed!")
 
